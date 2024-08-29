7f037dd899d9111b087796d120ed5840cbe9c9e7
CharmanderTheDev <144705748+CharmanderTheDev@users.noreply.github.com>
2024-08-29 01:51:44 +0000

    add gitignore, started creating map

---
+++ script.js
@@ -0,0 +1,20 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function locationDropdownOnClick() {
+    document.getElementById("locationDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+    const input = document.getElementById("locationDropdownInput");
+    const filter = input.value.toUpperCase();
+    const div = document.getElementById("locationDropdown");
+    const a = div.getElementsByTagName("a");
+    for (let i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+    } else {
+        a[i].style.display = "none";
+    }
+    }
+}
